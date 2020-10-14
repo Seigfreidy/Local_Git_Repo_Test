7b541a55a84b51e4a6a16873f3c639bc545f166f
Seigfreidy <yifan.a.chen@ericsson.com>
2020-10-14 14:37:00 +0800

    g++ test
    Change-Id: If7cf0655e9bfd57a3519fe34bfe92fed0e863dae

---
+++ For_Compile_Test/Print_Hello.cpp
@@ -0,0 +1,6 @@
+#include "Print_Hello.h"
+
+void Print_HelloWorld()
+{
+	std::cout << "Hello World!\n" << std::endl;
+}
---
+++ For_Compile_Test/main.cpp
@@ -0,0 +1,9 @@
+#include "Print_Hello.h"
+
+using namespace std;
+
+int main()
+{
+	Print_HelloWorld();
+	return 0;
+}
